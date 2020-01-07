0f855e9505f0c4ea0e6130e5a6ca0982b4e036fa
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2020-01-07 22:35:54 +0100

    passing chords datavia props

--- App.svelte
+++ App.svelte
@@ -1,8 +1,29 @@
 <script>
+  import Home from './Home.svelte'
+  import { ChordBox } from 'vexchords'
+
+  let showHome = true
+  let chosenChords = []
+  $: console.log(chosenChords)
+  function addChords(event) {
+    if (event.target.tagName === 'BUTTON') {
+      chosenChords = [...chosenChords, event.target.innerText]
+    }
+  }
 </script>
 <style>
 </style>
 
 <header>
-  <h1>GuitBetter</h1>
+  {#if showHome}
+    <h1>GuitBetter</h1>
+    <Home
+      start={() => { showHome = false }}
+      chosenChords={chosenChords}
+      addChords={addChords}
+    />
+  {:else}
+    <div>Chosen Chords YO! Let's start</div>
+    <div>{chosenChords.join(', ')}</div>
+  {/if}
 </header>
